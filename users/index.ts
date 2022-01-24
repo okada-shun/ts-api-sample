f8b09254143d9156ed1bcc85b4d60bdc7b37ebae
okada-shun <syun19981207@gmail.com>
2022-01-24 15:40:30 +0900

    :watermelon: Add: sample comment

--- users/index.ts
+++ users/index.ts
@@ -5,6 +5,8 @@ import * as handlers from "./handlers";
 export default async function (fastify: FastifyInstance): Promise<void> {
   fastify.get("/user/get/1", { schema: schemas.getUser1 }, handlers.getUser1);
   fastify.get("/user/get/2", { schema: schemas.getUser2 }, handlers.getUser2);
+  // aa
+  // bb
   fastify.get("/user/get/:userId", { schema: schemas.getUserById }, handlers.getUserById);
   fastify.post("/user/create", { schema: schemas.createUser }, handlers.createUser);
 }
